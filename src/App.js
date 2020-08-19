d87f8e48a426e9ff983593a4f3b9b81d3c2447b7
Ernesto Gutiérrez <ergutierrez39@gmail.com>
2020-08-19 04:57:08 -0600

    List the Menu containig the dishes.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Menu from './components/MenuComponent';
 import { Navbar, NavbarBrand } from 'reactstrap';
 
 class App extends Component {
     render () {
         return (
-            <div className="App">
+            <div>
                 <Navbar dark color="primary">
                     <div className="container">
                         <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
                     </div>
                 </Navbar>
+                <Menu />
             </div>
         );
     }
